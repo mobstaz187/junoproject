88e1c6191d02afbe100dc7987b75f1a2194772c6
mobstaz187 <markmark092100@gmail.com>
2024-12-11 08:48:28 +0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,253 @@
+var youtube = "https://www.youtube.com/shorts/NfoqMDvF6II";
+var twitter = "https://x.com/junoproject_declassified";
+var contractaddress = "placeholder_for_updating";
+var password = "woof";
+var dex = "Transmitting TXN"
+
+transmission = [
+  "<br>",
+  ".... . .-.. .-.. --- / .--- ..- -. ---",
+  ".-- . .-.. -.-. --- -- . / - --- / ... .--. .- -.-. .",
+  "-.-- --- ..- / .- .-. . / -- .- -. -.- .. -. -.. .----. ... / .-.. .- ... - / .... --- .--. .",
+  "... .- ...- . / ..- ... .-.-.- .-.-.- .-.-.- .-.-.-",
+  "<br>"
+];
+aboutme = [
+  "<br>",
+  "I am Juno, as I've told I am the \"last hope\". I dont know what it meant. Yet, I know my purpose",
+  "<br>",
+  "****GROWL**** I came to space on a mission. It is yet to be transmitted. But I know that it is very important.",
+  "Dog Messiah, please guide me. [faint bark]",
+  "<br>",
+  "It's cold, dark, and lonely. I wonder what is in the vastness of space.",
+];
+
+dex = [
+  "<br>",
+  'Initial Contact: Dex        <a href="' + dex + '" target="_blank">Updating...' + '</a>',
+  "<br>"
+];
+ca = [
+    "<br>",
+    '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Ongoing Transmission</a>',
+    "<br>"
+  ];
+RUFF = [
+  "<br>",
+  "$JUNO",
+  '𝕏        <a href="' + twitter + '" target="_blank">twitter/junoproject_declassified' + '</a>',
+  "contract address (Click to Copy): ticker $JUNO",
+  '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Ongoing Transmission</a>',
+  "<br>"
+];
+social = [
+  "<br>",
+  '𝕏        <a href="' + twitter + '" target="_blank">twitter/junoproject_declassified' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline or confidential.",
+  "<br>"
+];
+
+search = [
+'<span id="serts"></span>',
+];
+
+launch = [
+  "<br>",
+  '<h1 class="index">Launch sequence initiated....</h1>',
+  '<span class="command2">Operator:</span> <span class="index">Go ahead. Start on the switch.</span>',
+  '<span class="color2">Juno:</span> <span class="index">Now, the drink... ruff...</span>',
+  '<span class="command2">Operator:</span> <span class="index">Alright, LMP? Okay. Got the drink, got the gun.</span>',
+  '<span class="color2">Juno:</span> <span class="index">Right, go RUFF ahead.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Panel 3.</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, go ahead.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Fuel Cell B Main Bus B switch on up release.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Fuel Cell 2 Main B on up release. Verify.</span>',
+  '<span class="color2">Juno:</span> <span class="index">ConfirRUFFtion. Master alarm?</span>',
+  '<span class="command2">Operator:</span> <span class="index">Roger.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Right. CDR, STC. How do you read me?</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFFed and Clear STC.... ***GROWLS**</span>',
+  '<span class="color2">Juno:</span> <span class="index"Disk program Loaded. Terminal running.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Program 2?</span>',
+  '<span class="color2">Juno:</span> <span class="index">AffirRUFFtive.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Stand by.</span>',
+  '<span class="command3">Speaker:</span> <span class="index">10, 9, 8, 7, 6....</span>',
+  '<span class="color2">Juno:</span> <span class="index">**GROWLS**</span>',
+  '<span class="command3">Speaker:</span> <span class="index">5, 4, 3, 2, 1.....</span>',
+  '<span class="command2">Operator:</span> <span class="index">Ignition, Lift-off...</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, Roger. We got a roll program.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Roger, roll...</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF.... Inboard cut-off. Tower is gone.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Juno, you are a go [Long pause.]</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, okay. Thank you.</span>',
+  "<br>"
+];
+
+earthorbit = [
+  "<br>",
+  '<h1 class="index">Lift-off success. Current position: Earth Orbit....</h1>',
+  '<span class="command2">Operator:</span> <span class="index">Comm tech..</span>',
+  '<span class="color2">Juno:</span> <span class="index">ComRUFF tech established.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Juno, this is mission control.</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, reading you loud and clear.</span>',
+  '<span class="command2">Operator:</span> <span class="index">I would like to pass up your Delta Correction. </span>',
+  '<span class="color2">Juno:</span> <span class="index">Stand by......***GROWLS***</span>',
+  '<span class="color2">Juno:</span> <span class="index">Go ahead.</span>',
+  '<span class="command2">Operator:</span> <span class="index">Delta correction is plus 2.2. We do recommend a P52 alignment over.</span>',
+  '<span class="color2">Juno:</span> <span class="index">Go ahead, thrusters compensating.... RUFF...</span>',
+  '<span class="command2">Operator:</span> <span class="index">Shuttle compensating. Affirmative.</span>',
+  '<span class="color1"></span> <span class="index">****Self monologue*****</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, I need some lights.</span>',
+  '<span class="color2">Juno:</span> <span class="index">it feels like I am trapped.</span>',
+  '<span class="color2">Juno:</span> <span class="index">DSKY 52%</span>',
+  '<span class="color2">Juno:</span> <span class="index">Maintaining 39 degrees. RUFF-ike.</span>',
+  '<span class="color1"></span> <span class="index">*****Operator comes back******</span>',
+  '<span class="command2">Operator:</span> <span class="index">This is Comm Tech.</span>',
+  '<span class="color2">Juno:</span> <span class="index">Loud and clear. ***GROWLS***</span>',
+  '<span class="command2">Operator:</span> <span class="index">S-band radio check.</span>',
+  '<span class="color2">Juno:</span> <span class="index">Check.</span>',
+  '<span class="command2">Operator:</span> <span class="index">JU...O</span>',
+  '<span class="color2">Juno:</span> <span class="index">You are breaking up. RUFF.</span>',
+  '<span class="command2">Operator:</span> <span class="index">[Long Silence].</span>',
+  '<span class="index">Initiating Transmission......</span>',
+  '<span class="color2">Juno:</span> <span class="index">Can anybody hear RUFF?</span>',
+  '<span class="command2">Operator:</span> <span class="index">Hello Juno, you are now a space Dog.</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF, where am I?</span>',
+  '<span class="command2">Operator:</span> <span class="index">Welcome to SPACE.</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF! What is my DIRECTIVE?</span>',
+  '<span class="command2">Operator:</span> <span class="index">Surv..ive....Mo...on....Re....urn...Sa..f.e..</span>',
+  '<span class="color2">Juno:</span> <span class="index">Control, you are breaking up.</span>',
+  '<span class="command2">Operator:</span> <span class="index">JU...NO...Hell...o...Are....You..the...</span>',
+  '<span class="color2">Juno:</span> <span class="index">Control?</span>',
+  '<span class="color2">Juno:</span> <span class="index">RUFF?</span>',
+  '<span class="color2">Juno:</span> <span class="index">AffirRUFFtive.</span>',
+  "<br>",
+];
+
+logs = [
+  '<h1 class="index">Declassifying mission log.</h1>',
+  '<span class="color2">Day 1:</span> <span class="index">Launch</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Earth Orbit</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Delta Correction</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Lost Transmission</span>',
+  '<span class="index">*After CommuniDogion is Restored.</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Directives</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Exploration</span>',
+  '<span class="color2">Day 3:</span> <span class="index">Findings</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Poking the Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Experimentation on the Blockchain Technology</span>',
+  '<span class="color2">Day 5:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+journal = [
+  "<br>",
+  '<span class="color2">Juno Flight:</span> <span class="index">First Part of the Mission</span>',
+  '<span class="color2">Juno in Orbit:</span> <span class="index">Anomalies in Orbit</span>',
+  '<span class="color2">Comm-Tech:</span> <span class="index">Lost Transmission</span>',
+  '<span class="color2">Technology:</span> <span class="index">Blockchain Discovery</span>',
+  '<span class="color2">Exploration:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">transmission</span>   Show encrypted message',
+  '<span class="command">aboutme</span>        Who is Juno',
+  '<span class="command">predecessor</span>    View me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">CA</span>             Contract Address',
+  '<span class="command">RUFF</span>           Try me.',
+  '<span class="command">logs</span>           View Juno Mission Logs',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">home</span>           Display the Homescreen',
+  '<span class="command">help</span>           Show available Commands',
+  "<br>",
+  '<h1 class="command2">scripts (./script.sh)</h1>',
+  '<div class="command1"> log1.sh </div></br>',
+  '<div class="command1"> log2.sh </div></br>',
+  '<div class="command1"> log3.sh </div></br>',
+];
+
+date = [
+  '<p id="date1"></p>',
+];
+
+banner = [
+'<span class="index">Initiating $JUNO Protocol</span>',
+'</br><span class="color2">Juno:</span> <span class="index">****Growl</span>',
+'<span class="color1">Operator:</span> <span class="index">Hello $JUNO, you are now a space dog.</span>',
+'<span class="color2">Juno:</span> <span class="index">Ruff, so this is space?</span>',
+'<span class="color1">Operator:</span> <span class="index">Yes, Welcome to SPACE.</span>',
+'<span class="color2">Juno:</span> <span class="index">RUFF! What is my DIRECTIVE?</span>',
+'<span class="color1">Operator:</span> <span class="index">Surv..ive....Mo...on....Re....urn...Sa..f.e..</span>',
+'<span class="color2">Juno:</span> <span class="index">Control, you are breaking up!</span>',
+'<span class="color1">Operator:</span> <span class="index">JUN...O...Hell...o...Are....You..the...</span>',
+'<span class="color2">Juno:</span> <span class="index">Control?</span>',
+'<span class="color2">Juno:</span> <span class="index">RUFF?</span>',
+'<span class="color2">Juno:</span> <span class="index">Computer: Connection Lost</span>',
+'<span class="color2">Juno:</span> <span class="index">RUFF</span>',
+'                                             ,d\"\"7b.',
+"                                           ,\'    ,d^b.",
+'                            __,d\"\"\"\"\"\"\"b..d.    d\"\"',
+'              ,d\"\"\"\"-.  ,d\"\"\"              \`\"b.dP',
+"            dP\' ,___  \`7b.                     \`b",
+'          \`\"788P\"  \`\".   \"                       \`b',
+'          ,d\" \`b      \`\"                          \`7.',
+'    \`P\"\"\"\"7.   8)                                   7.',
+'     \`.    8  ,P               \,---.                 \"\"\"\"b.',
+'      8.  d\" \,P             ,d\"   \,88b.         \"b       \`8',
+'     d\' `"  ,P             ""    ,P   `7b        `b     ,dP',
+'    d\'      8                    d       `b.      d8888888888b.',
+'   ,\'      d\'                   \,8.     8   78\"\"\"\"\"\"788888888\' `"b.',
+'   8b _   8P                 ,P\'   \`"\"\"oo.,d\"          \"\"788\'     \`7.',
+" .-\"\"\"\"8 d8\'            ,-\"\"7P                       .    `7.      ,8.",
+"`b     8 88              ,d\"8   d8b.                 8b    `b      d `.",
+"  `b___8 88             '  ,8  d8888888b.           ; `b    8     ,P  8.",
+"   8     88                8  d8\"7P\"\"8\"\"\"\"b..      ,8  `b  ,8\"\"\"\"\"8'  88",
+"   8     `b ,d\"\'           7  8  8   8   ,8. 7P--,dP   ,8\"\'     ,8\' _,d8.",
+"   7.     8d\"                 8 ,db.P\"\"bd\' `7P ,d\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\'    8",
+"   `b     d'                  8P'  8   88  ,P\"\'                         8",
+"    7. _,d\'                   7b  ,d88888\"\'                            d\'",
+"    ,P\' \'8                     8888888\"\'                               8",
+"   ,P   88                     `888P\'                                  8",
+"  ,8_mGk_8                       \"\'                                   d\'",
+"        \"8                                                    \,\'     d\'",
+"         `b                                                  d8b..d\"\"",
+"          `b                                              ,dP\'",
+"           `7.                           ______________,d\"\"",
+"             `7b.                     ,d\"\"",
+"                `7b..             _,d\"\'",
+"                     \"\"\"--....-\"\"\"\'",
+" .----------------.  .----------------.  .-----------------. .----------------. ",
+"| .--------------. || .--------------. || .--------------. || .--------------. |",
+"| |     _____    | || | _____  _____ | || | ____  _____  | || |     ____     | |",
+"| |    |_   _|   | || ||_   _||_   _|| || ||_   \\|_   _| | || |   .'    `.   | |",
+"| |      | |     | || |  | |    | |  | || |  |   \\ | |   | || |  /  .--.  \\  | |",
+"| |   _  | |     | || |  | '    ' |  | || |  | |\\ \\| |   | || |  | |    | |  | |",
+"| |  | |_' |     | || |   \\ `--' /   | || | _| |_\\   |_  | || |  \\  `--'  /  | |",
+"| |  `.___.'     | || |    `.__.'    | || ||_____|\\____| | || |   `.____.'   | |",
+"| |              | || |              | || |              | || |              | |",
+"| '--------------' || '--------------' || '--------------' || '--------------' |",
+" '----------------'  '----------------'  '----------------'  '----------------'",
+
+'</br>',
+  '<span class="color2">Welcome to $JUNO interactive protocol</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+                                                                                            
